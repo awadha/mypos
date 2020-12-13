49c42ae390154e30f0af9640bbbbf5979073b828
Awad <whgp1000000@gmail.com>
2020-12-13 12:18:00 +0300

    first commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.dashboard.app')
+
+@section('content')
+
+    <div class="content-wrapper">
+
+        <section class="content-header">
+
+            <h1>@lang('site.dashboard')</h1>
+
+            <ol class="breadcrumb">
+                <li class="active"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</li>
+            </ol>
+        </section>
+
+        <section class="content">
+            t7uy8i9
+
+            <div class="box box-solid">
+
+                <div class="box-header">
+                    <h3 class="box-title">Sales Graph</h3>
+                </div>
+                <div class="box-body border-radius-none">
+                    <div class="chart" id="line-chart" style="height: 250px;"></div>
+                </div>
+                <!-- /.box-body -->
+            </div>
+
+        </section><!-- end of content -->
+
+    </div><!-- end of content wrapper -->
+
+
+@endsection
+
+@push('scripts')
+
+    <script>
+
+
+    </script>
+
+@endpush
